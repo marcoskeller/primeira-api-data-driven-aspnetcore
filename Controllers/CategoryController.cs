656f34dfe23f78f464f2c26b58e7477856bade01
mfonseca.squadra <marcos.fonseca@squadra.com.br>
2022-01-20 13:52:32 -0300

    Projeto na versao SDK 3.1

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using primeira_api_data_driven_asp;
+
+[Route("categories")]
+public class CategoryController : ControllerBase
+{
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<List<Category>>> Get()
+    {
+        return new List<Category>();
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> GetById(int id)
+    {
+        return new Category();
+    }
+
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        else
+        {
+            return Ok(model);
+        }       
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task<ActionResult<List<Category>>> Put(int id, [FromBody] Category model)
+    {
+        if(model.Id != id)
+        {
+            return NotFound(new { message = "Categoria não encontrada." });
+        }
+        else if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        else
+        {
+            return Ok(model);
+        }       
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult<List<Category>>> Delete()
+    {
+        return Ok();
+    }
+}
+
