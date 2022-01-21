0d0aa79233682be98e0c52f96d316d5a3b44cff5
marcoskeller <marcoskeller@hotmail.com>
2022-01-20 22:28:51 -0300

    Inserido a configuracao do DataContexto no Startup

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace primeira_api_data_driven_asp.net
         {
             services.AddControllers();
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("DataBase"));
+            services.AddScoped<DataContext, DataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
